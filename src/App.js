1ac9724db80609a05378f48f5cff7ba378317b10
Elvan H <elvanhd@gmail.com>
2021-11-30 21:12:18 +0700

    adding a users list component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      name: 'John',
+      age: 30
+    },
+    {
+      id: 2,
+      name: 'Sara',
+      age: 25
+    },
+    {
+      id: 3,
+      name: 'William',
+      age: 18
+    }
+  ];
+
   return (
     <div>
       <AddUser />
+      <UsersList users={users} />
     </div>
   );
 }
